7a7981763f83b104308c79624e8a7845f41e5843
zeiverking <zeiverking@gmail.com>
2025-12-15 13:21:49 -0600

    revert

--- app/app.py
+++ app/app.py
@@ -56,7 +56,6 @@ class INEData(BaseModel):
     apellido_paterno: Optional[str] = None
     apellido_materno: Optional[str] = None
     nombres: Optional[str] = None
-    sexo: Optional[str] = None
     direccion: Optional[str] = None  # viene de "domicilio"
     codigo_postal: Optional[str] = None
     curp: Optional[str] = None  # Validacion con api de curp
@@ -333,7 +332,6 @@ async def parse_ine(
             apellido_paterno=result.get("apellido_paterno"),
             apellido_materno=result.get("apellido_materno"),
             nombres=result.get("nombres"),
-            sexo=result.get("sexo"),
             direccion=result.get("domicilio"),
             codigo_postal=result.get("codigo_postal"),
             curp=result.get("curp"),
