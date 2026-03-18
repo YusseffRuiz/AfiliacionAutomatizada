82713eb09aa07966bb7d85a66a19e83ec5690cd3
zeiverking <zeiverking@gmail.com>
2026-03-18 14:17:18 -0600

    logs por mes updated

--- app/utils/stats.py
+++ app/utils/stats.py
@@ -11,15 +11,16 @@ class StatsManager:
         self.current_file = os.path.join(self.file_path, "usage_stats_global.json")
         self.lock = Lock()
         os.makedirs(self.file_path, exist_ok=True)
-        self._ensure_file(self.current_file)
+        self._ensure_file()
 
-    def _get_montly_path(self):
+    def _get_monthly_path(self):
         now = datetime.datetime.now()
         return os.path.join(self.file_path, f"stats_{now.year}_{now.month:02d}.json")
 
-    def _ensure_file(self, file_path: str):
+    @staticmethod
+    def _ensure_file(path: str):
         # SI EL ARCHIVO YA EXISTE, NO HACE NADA. Si es un archivo nuevo, inicia en 0.
-        if not os.path.exists(file_path):
+        if not os.path.exists(path):
             initial_stats = {
                 "total_requests": 0,
                 "total_processing_time": 0.0,  # Suma de todos los segundos
@@ -28,7 +29,7 @@ class StatsManager:
                 "outcomes": {"success": 0, "failure": 0},
                 "last_update": str(datetime.datetime.now())
             }
-            with open(file_path, "w") as f:
+            with open(path, "w") as f:
                 json.dump(initial_stats, f, indent=4)
 
     def log_usage(self, engine: str, success: bool, response_time: float):
